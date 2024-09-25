0723a13327284f9a54cb24d37cfde2afa1802bfb
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-25 22:57:02 +0200

    Update config again

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://microfox.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/slice-of-life/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'Admer456',
